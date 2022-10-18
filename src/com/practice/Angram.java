a3305cc234751254825cfc35c9d16efc3200ee76
ahily <ahily@CGTECH>
2022-10-18 18:28:56 +0530

    first commit

---
+++ src/com/practice/Angram.java
@@ -0,0 +1,29 @@
+package com.practice;
+
+import java.util.Arrays;
+
+public class Angram {
+
+	public static void main(String[] args) {
+		
+		String str="keep";
+		String stra="Peek";
+		boolean	status=false;
+		if(str.length()!=stra.length()) {
+			
+		}else {
+		char arr1[]=str.toLowerCase().toCharArray();
+		char arr2[]=stra.toLowerCase().toCharArray();
+	
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		status=Arrays.equals(arr1, arr2);
+		}	
+		if(status==true) {
+			System.out.println("The given strings are anagram"); 
+		}else {
+			System.out.println("The given strings are not anagram");
+		}
+	}
+
+}
